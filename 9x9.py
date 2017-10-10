ea6537dfc3ff34248efd04e11706425d1d28cd18
LiWei <mono@nei.local>
2017-10-10 14:39:49 +0800

    change 10-12

--- 9x9.py
+++ 9x9.py
@@ -7,6 +7,6 @@ Created on Fri Oct  6 17:13:45 2017
 """
 
 for i in range(1,10):
-      for j in range(1,10):
+      for j in range(1,12):
             print(i, '*', j, '=', i*j, '\t', sep='',end='')
       print()
