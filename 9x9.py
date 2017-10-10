24b04125fc532a3779719b6b26e77e0eb97ce2d4
LiWei <mono@nei.local>
2017-10-10 14:36:35 +0800

    python

---
+++ 9x9.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  6 17:13:45 2017
+
+@author: mono
+"""
+
+for i in range(1,10):
+      for j in range(1,10):
+            print(i, '*', j, '=', i*j, '\t', sep='',end='')
+      print()
